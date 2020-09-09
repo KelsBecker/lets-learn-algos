9fd859c6906204103f274d329f321e1505f623c1
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-09-09 11:34:33 -0400

    add binary search refactor

--- Searching/script.js
+++ Searching/script.js
@@ -51,20 +51,16 @@ function linearSearch(array, val){
 //if I never find the value, return -1
 
 function binarySearch(sortedArr, val){
-  let start = 0;
-  let end = sortedArr.length -1;
-  let middle = Math.floor((start + end) / 2);
+  let start = 0
+  let end = sortedArr.length - 1
+  let middle = Math.floor((start + end) / 2)
   while(sortedArr[val] !== middle && start <= end){
-    if(val < sortedArr[middle]){
-      end = middle - 1
-    } else {
-      start = middle +1
-    }
+    if(val < sortedArr[middle]) end = middle - 1;
+    else start = middle + 1
     middle = Math.floor((start + end) / 2)
   }
-  if(sortedArr[middle] === val){
-    return middle
-  } 
-  return -1
+  return sortedArr[middle] === val ? middle : -1
 }
 // console.log(binarySearch([1,2,3,4,5], 2))
+// console.log(binarySearch([1,4,5,7,9,10,15,17,19,23], 19))
+// console.log(binarySearch([2,3,4,6,8,9,11,15,28,33,45,56,67], 29))
