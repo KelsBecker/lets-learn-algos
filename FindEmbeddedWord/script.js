2251dbcda79998a12523b85e834883e7c9fe3f9f
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-09-19 14:38:28 -0400

    add embedded word function

--- FindEmbeddedWord/script.js
+++ FindEmbeddedWord/script.js
@@ -1,7 +1,13 @@
 /*
-You are running a classroom and suspect that some of your students are passing around the answers to multiple choice questions disguised as random strings.
+You are running a classroom and suspect 
+that some of your students are passing around the answers to multiple choice questions 
+disguised as random strings.
 
-Your task is to write a function that, given a list of words and a string, finds and returns the word in the list that is scrambled up inside the string, if any exists. There will be at most one matching word. The letters don't need to be contiguous.
+Your task is to write a function that, 
+given a list of words and a string, 
+finds and returns the word in the list that is scrambled up inside the string, 
+if any exists. 
+There will be at most one matching word. The letters don't need to be contiguous.
 
 Example:
 words = ['cat', 'dog', 'bird', 'car', 'ax', 'baby']
@@ -27,23 +33,13 @@ const string2 = "tbcanihjs";
 const string3 = "baykkjl";
 const string4 = "bbabylkkj";
 
-// const find_embedded_word = (wordArray, string) => {
-//   wordArray.forEach((word) => {
-//     let match = 0
-//     console.log(word)
-//     for(let i = 0; i <= word.length; i++){
-//       console.log("->", word[i])
-//       for(let j = 0; j <= string.length; j++){
-//           console.log("-->", word[i], string[j])
-//         if(word[i] === string[j]) {
-//           match++
-//           console.log("--> match increases!")
-//         }
-//           if(match === word.length) return word;
-//           continue
-//       }
-//     }
-//   })
-//   return null
-// }
-// console.log(find_embedded_word(['tac'], string1))
+const find_embedded_word = (wordArray, string) => {
+  for(let i = 0; i < wordArray.length; i++){
+    let word = wordArray[i]
+    for(let j = 0; j <= word.length; j++){
+
+    }
+  }
+  return 'None'
+}
+console.log(find_embedded_word(words, string1))
