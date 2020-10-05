361b64aea7c4416314deeb439d690accf3fdf960
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-10-05 18:30:29 -0400

    add reverse function to singly linked list

--- DataStructures/Singly-Linked-List/script.js
+++ DataStructures/Singly-Linked-List/script.js
@@ -141,6 +141,22 @@ class SinglyLinkedList {
     }
   }
 
+//reverse the linked list in place
+  reverse(){
+    let node = this.head
+    this.head = this.tail
+    this.tail = node
+    let next;
+    let prev = null;
+    for(let i = 0; i < this.length; i++){
+      next = node.next
+      node.next = prev
+      prev = node
+      node = next
+    }
+    return this  
+  }
+
 }
 
 
