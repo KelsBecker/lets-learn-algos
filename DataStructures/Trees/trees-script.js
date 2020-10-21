01db3d036b5bdea0d516e7418c3c7bd16d6dd50b
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-10-21 18:58:13 -0400

    add insert function to binary search tree

--- DataStructures/Trees/trees-script.js
+++ DataStructures/Trees/trees-script.js
@@ -39,9 +39,31 @@
     }
 
   //add a node to the tree
-    insert(){
-
+    insert(value){
+      let newNode = new Node(value)
+      if(!this.root){
+        this.root = newNode
+        return this
+      } 
+      let current = this.root
+        while(true){
+          if(value === current.value) return undefined
+          if(value < current.value){
+            if(current.left === null){
+              current.left = newNode
+              return this
+            }
+            current = current.left
+          } else {
+            if(current.right === null){
+              current.right = newNode
+              return this
+            }
+            current = current.right
+          }
+        }
     }
+
   }
 
   let tree = new BinarySearchTree()
